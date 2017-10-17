47ffaaf01c5e781d210a67e5e42d37fa8b8e2660
Michelle Cinello <m.cinello@gmail.com>
2017-10-17 16:40:31 -0400

    changed column 'quantity' in 'parts' table from an integer to a decimal type

--- db/migrate/20171017184136_add_quantity_to_parts.rb
+++ db/migrate/20171017184136_add_quantity_to_parts.rb
@@ -4,4 +4,12 @@ class AddQuantityToParts < ActiveRecord::Migration[5.0]
       t.integer :quantity
     end
   end
+    # 
+    # def up #method to migration the column type forward
+    #   change_column :parts, :quantity, :decimal
+    # end
+    #
+    # def down #method to rollback the column type to its previous state
+    #   change_column :parts, :quantity, :integer
+    # end
 end
---
+++ db/migrate/20171017203833_change_add_quantity_to_parts.rb
@@ -0,0 +1,9 @@
+class ChangeAddQuantityToParts < ActiveRecord::Migration[5.0]
+  def up #method to migration the column type forward
+    change_column :parts, :quantity, :decimal
+  end
+
+  def down #method to rollback the column type to its previous state
+    change_column :parts, :quantity, :integer
+  end
+end
